2374ff5536c3628764eaa8718d8c5907f37f4411
Tomtom-dev <54830680+Tomtom-dev@users.noreply.github.com>
2021-04-29 17:23:05 +0200

    add middleware function to parse the post request

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const express = require ('express');
-
 const app= express()
 const port= 3000;
 
 app.get('/', (req,res) =>{
-    res.send('hi there')
+    res.send(`
+    <div>
+        <form method="POST">
+            <input name="email" placeholder="email" />
+            <input name="password" placeholder="password" />
+            <input name="passwordConfirmation" placeholder="password confirmation" />
+            <button> Signup </button>
+        </form>
+    </div>
+    `)
+})
+
+// Middleware function
+const bodyParser = (req, res, next) => {
+    // get access to email, password, password confirmation
+    if (req.method === 'POST'){
+        req.on('data', data =>{
+            const parsed = data.toString('utf8').split('&')
+            const formData= {};
+            for (let pair of parsed){
+                const [key,value] = pair.split('=');
+                formData[key]=value;
+            }
+            req.body = formData;
+            next();
+        })
+    } else {
+        next();
+    }
+}
+
+app.post('/', bodyParser, (req,res) => {
+    console.log(req.body);
+    res.send('account created !!')
 })
 
 app.listen(port, () =>{
