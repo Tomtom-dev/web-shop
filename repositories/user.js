8699928f83cc8a0748e17cb1f6e5d278a814268e
Tomtom-dev <54830680+Tomtom-dev@users.noreply.github.com>
2021-04-30 16:08:44 +0200

    add method to create and writeall user and test it

--- repositories/user.js
+++ repositories/user.js
@@ -1,4 +1,3 @@
-const { createCipher } = require('crypto');
 const fs= require('fs')
 
 class UsersRepository {
@@ -14,6 +13,31 @@ class UsersRepository {
             fs.writeFileSync(this.filename, '[]')
         }
     }
+    async getAll(){
+        // Open the file called this.filename
+        return JSON.parse(await fs.promises.readFile(this.filename, {
+            encoding :'utf8'
+        }));
+    }
+
+    async create(attribute){
+        const records = await this.getAll();
+        records.push(attribute);
+        // write the updates 'records' array back to this.filename
+        await this.writeAll(records)
+    }
+
+    async writeAll (records){
+        await fs.promises.writeFile(this.filename, JSON.stringify(records,null, 2))
+    }
 }
 
-new UsersRepository('users.json');
+const test = async () =>{
+    const repo = new UsersRepository('users.json');
+    
+    await repo.create({email: "tes@this.com", password: '1234', })
+    const users = await repo.getAll()
+    console.log(users);
+}
+
+test()
